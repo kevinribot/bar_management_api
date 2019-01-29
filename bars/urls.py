2ce8d16e95a9ced11558525c87fa34e2c50774ac
Ribot72 <kribot@laposte.net>
2019-01-29 11:02:48 +0100

    Update urls.py
    Ajout de commentaire

--- bars/urls.py
+++ bars/urls.py
@@ -1,7 +1,8 @@
 import bars.receivers
-from .views import ReferenceList, BarList, StockList, MenuList, RankList,OrderList, OrderDetail, OrderCreate, OrderItemList
+from .views import ReferenceList, BarList, StockList, MenuList, RankList,OrderList, OrderDetail, OrderCreate
 from django.urls import path
 
+# Création des urls accessibles
 urlpatterns = [
     path("api/references/", ReferenceList.as_view(), name="references_list"),
     path("api/bars/", BarList.as_view(), name="bars_list"),
@@ -11,7 +12,6 @@ urlpatterns = [
     path('api/menu/<int:bar>/', MenuList.as_view(), name="menu_detail"),
     path('api/order/<int:bar>/', OrderCreate.as_view(), name="order_create"),
     path('api/orders/', OrderList.as_view(), name="order_list"),
-    path('api/orderitems/', OrderItemList.as_view(), name="orderitem_list"),
     path('api/order/<int:pk>/', OrderDetail.as_view(), name="order_detail"),
 ]
 
