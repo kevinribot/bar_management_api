db233e3e4d54951aa2b515dbff0280f406300bb2
Ribot72 <kribot@laposte.net>
2019-01-31 10:35:12 +0100

    Update permissions.py
    Ajout de PostByClientAndGetByUserPermission

--- bars/permissions.py
+++ bars/permissions.py
@@ -14,3 +14,17 @@ class OnlyUserAndStaffPermission(permissions.BasePermission):
 
         return False
 
+
+# Création d'une permission qui permet seulement au utilisateur authentifi
+class PostByClientAndGetByUserPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return request.user and request.user.is_authenticated
+
+        if request.method in ('POST', 'DELETE', 'PUT', 'PATCH'):
+            return not (request.user and request.user.is_authenticated)
+
+        return False
+
